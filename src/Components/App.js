c1b646a7f4edcff3cf8a02cdc0f79567ab9cc01d
khushbu19998@outlook.com <khushbu19998@outlook.com>
2024-05-18 18:41:06 +0530

    second commit

---
+++ src/Components/App.js
@@ -0,0 +1,27 @@
+
+import '../Css/App.css'
+import { useRef,useState } from 'react';
+import Bmi from './Bmi';
+function App() {
+  let nameinput=useRef();
+  const [name,setnamae]=useState('')
+  const [btn,setbtn]=useState(true)
+  function nameHendler(){
+   setnamae(nameinput.current.value);
+   nameinput.current.value=''
+   setbtn(false)
+  }
+  return (
+    <div>
+    <div className="container">
+      <h1>Welcomr to bootcamp {name}!</h1>
+     <input ref={nameinput} placeholder='enter your name' type='text'/>
+     <button onClick={nameHendler} className={btn==true?'btn' :'d-none'}>submit</button>
+     </div>
+     <Bmi/>
+     
+    </div>
+  );4
+}
+
+export default App;
---
+++ src/Components/Bmi.js
@@ -0,0 +1,47 @@
+
+import '../Css/App.css'
+import { useRef,useState } from 'react';
+function Bmi() {
+  let heightinput=useRef();
+  let weighttinput=useRef();
+//   const [name,setnamae]=useState('')
+  const [bmi,setbmi]=useState('')
+  const [bminumber,setbminumber]=useState('')
+  function calculateBmiHendler(){
+    let bmicaculate=(weighttinput.current.value) / (heightinput.current.value/100 * heightinput.current.value/100 )
+   setbminumber(bmicaculate);
+   if(bmicaculate<=18.5){
+    setbmi('Underweight');
+   }
+    else if(bmicaculate>18.5 && bmicaculate<=24.9){
+        setbmi('Normal');
+    }
+    else if(bmicaculate>25 && bmicaculate<=29.9){
+        setbmi('Overweight');
+    }
+    else if(bmicaculate>30 && bmicaculate<=35){
+        setbmi('Obesity');
+    }
+    else{
+        setbmi('Severe Obesity');
+    }
+   
+   heightinput.current.value='';
+   weighttinput.current.value=''
+//    setbtn(false)
+  }
+  return (
+    <div className="container1">
+       <h1>{bmi} </h1>
+      <h3>Calculate your bmi💪</h3>
+      <h4 className={bminumber == ''?'d-none' :""}>bmi={bminumber}</h4>
+      <div className='flex'>
+     <input ref={heightinput} placeholder='enter your height' type='text'/>
+     <input ref={weighttinput} placeholder='enter your weight' type='text'/>
+     </div>
+     <button onClick={calculateBmiHendler} className='btn1'>submit</button>
+    </div>
+  );
+}
+
+export default Bmi;
